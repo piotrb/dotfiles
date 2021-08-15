9159fe6243a10b0977710442dfa05dea213d8899
Piotr Banasik <piotr.banasik@gmail.com>
2021-08-15 14:44:16 -0700

    Fix ssh for linux and openssh mode

--- profile.rc.d/zz_ssh.sh
+++ profile.rc.d/zz_ssh.sh
@@ -1,12 +1,16 @@
 SSH_AGENT_MODE=Auto
 
+function which_s() {
+  env which $1 2>&1 >/dev/null && return 0 || return 1
+}
+
 function detect_mode() {
   if [ -e ~/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh ] && [ -e /Applications/Secretive.app ]; then
     SSH_AGENT_MODE=Secretive
     return
   fi
 
-  if env which -s ssh-agent; then
+  if which_s ssh-agent; then
     if { ssh-add -h 2>&1| grep "in your keychain" -q }; then
       SSH_AGENT_MODE=SSH-MAC
     else
@@ -15,7 +19,7 @@ function detect_mode() {
     return
   fi
 
-  if env which -s gpg-agent; then
+  if which_s gpg-agent; then
     SSH_AGENT_MODE=GPG
     return
   fi
@@ -44,10 +48,17 @@ case $SSH_AGENT_MODE in
     fi
     ;;
   SSH)
-    echo "Todo: how do we add all keys in openssh mode?"
-    # if [[ $(ssh-add -l | grep -v "no identities" | wc -l) -lt 1 ]]; then
-      # ssh-add -k
-    # fi
+    if [ -z "$(pgrep ssh-agent)" ]; then
+      rm -rf /tmp/ssh-*
+      eval $(ssh-agent -s) > /dev/null
+    else
+      export SSH_AGENT_PID=$(pgrep ssh-agent)
+      export SSH_AUTH_SOCK=$(find /tmp/ssh-* -name agent.*)
+    fi
+
+    if [[ $(ssh-add -l | grep -v "no identities" | wc -l) -lt 1 ]]; then
+      ssh-add -k
+    fi
     ;;
   SSH-MAC)
     if [[ $(ssh-add -l | grep -v "no identities" | wc -l) -lt 1 ]]; then
