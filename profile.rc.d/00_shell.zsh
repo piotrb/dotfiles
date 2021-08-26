450a429ff873d87b47bb288638e65b5cb26f6982
Piotr Banasik <piotr.banasik@gmail.com>
2021-08-25 17:04:20 -0700

    Make starship and direnv more "optional"

--- profile.rc.d/00_shell.zsh
+++ profile.rc.d/00_shell.zsh
@@ -1,1 +1,5 @@
-eval "$(starship init zsh)"
+if which starship 2>/dev/null 1>/dev/null; then
+  eval "$(starship init zsh)"
+else
+  echo "starship not installed"
+fi
--- profile.rc.d/direnv.zsh
+++ profile.rc.d/direnv.zsh
@@ -1,1 +1,5 @@
-eval "$(direnv hook zsh)"
+if which direnv 2>/dev/null 1>/dev/null; then
+  eval "$(direnv hook zsh)"
+else
+  echo "direnv not installed"
+fi
