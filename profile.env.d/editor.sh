900518d7d6b3de6e6a2c03d3490a6cef8fdc9522
Piotr Banasik <piotr.banasik@gmail.com>
2021-12-02 17:32:17 +0000

    Make editor envs set conditionally unless previously set

--- profile.env.d/editor.sh
+++ profile.env.d/editor.sh
@@ -1,5 +1,14 @@
-export EDITOR=vim
-export GIT_EDITOR=vim
-export BUNDLER_EDITOR=code
+if [ -z ${EDITOR+x} ]; then
+  export EDITOR=vim
+fi
+
+if [ -z ${GIT_EDITOR+x} ]; then
+  export GIT_EDITOR=vim
+fi
+
+if [ -z ${BUNDLER_EDITOR+x} ]; then
+  export BUNDLER_EDITOR=code
+fi
+
 #export EDITOR="code -w"
 #export GIT_EDITOR="code -w"
