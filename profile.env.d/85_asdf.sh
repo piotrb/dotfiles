82e4b13075719c7579b4df99178ae615e660616b
Piotr Banasik <piotr@jane.app>
2023-01-04 10:42:58 -0800

    Tweaks

--- profile.env.d/85_asdf.sh
+++ profile.env.d/85_asdf.sh
@@ -1,13 +1,15 @@
 if [ -x /usr/local/bin/brew ]; then
-  . $(brew --prefix asdf)/asdf.sh
+  . $(brew --prefix asdf)/libexec/asdf.sh
 elif [ -e $HOME/.asdf/asdf.sh ]; then
   . $HOME/.asdf/asdf.sh
 fi
 
-if [ ! -z "${ASDF_DIR}" ]; then
-  # append completions to fpath
-  fpath=(${ASDF_DIR}/completions $fpath)
-  # initialise completions with ZSH's compinit
-  autoload -Uz compinit
-  compinit
+if [ -n "$ZSH_VERSION" ]; then
+  if [ ! -z "${ASDF_DIR}" ]; then
+    # append completions to fpath
+    fpath=(${ASDF_DIR}/completions $fpath)
+    # initialise completions with ZSH's compinit
+    autoload -Uz compinit
+    compinit
+  fi
 fi
