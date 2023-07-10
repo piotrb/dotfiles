5ceca6c1c30084d2296b066be8594f2116e5dc0e
Piotr Banasik <piotr@jane.app>
2023-07-10 10:22:56 -0700

    Add `aw` wrapper around aws-vault

--- profile.rc.d/aws.sh
+++ profile.rc.d/aws.sh
@@ -1,8 +1,16 @@
-function _vault() {
+function aw() {
   if [ -e  $AWS_VAULT_PROFILE ]; then
     echo "must set AWS_VAULT_PROFILE"
     return 1
   fi
-  aws-vault exec $AWS_VAULT_PROFILE --session-ttl=3h --assume-role-ttl=4h -- "$@"
+  aws-vault exec $AWS_VAULT_PROFILE --duration=4h -- "$@"
 }
+
+export AWS_PAGER=""
+
+# if on zsh, set up completion
+if [ -n "$ZSH_VERSION" ]; then
+  # use exec's completion syntax for aw
+  compdef _exec aw
+fi
 
